3d8e8a1054dc4df76c0282b5bd1825b3c27bb6b3
Suriya Dorsen <s4820847@gmail.com>
2020-03-22 09:37:56 +0700

    sec2

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 let express = require('express') 
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express() 
 
@@ -9,11 +12,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 require('./routes')(app)
 app.get('/status', function (req, res ){ res.send('Hello nodejs server')
 })
-app.get('/hello/:person', function (req,res) { console.log('hello - ' + req.params.person) 
-res.send('sey hello with ' + req.params.person)
+app.get('/hello/:person', function (req,res) { 
+    console.log('hello - ' + req.params.person) 
+    res.send('sey hello with ' + req.params.person)
 })
-app.post('/hello', function (req, res) { res.send('OK you post - ' + req.body.name)
+app.post('/hello', function (req, res) { 
+    res.send('OK you post - ' + req.body.name)
 })
-let port = 8081
-app.listen(port, function () { console.log('server running on ' + port)
+
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => { 
+    app.listen(port, function () {
+console.log('Server running on ' + port) })
 })
